611dd07df8afba7487d9ee73090647daf06899e3
Lars Greiss <kufikugel@googlemail.com>
2016-11-03 15:02:19 +0800

    HeadsUp: add timeout option (2/2)
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/headsup/HeadsUpSettings.java
+++ src/com/pixeldust/settings/headsup/HeadsUpSettings.java
@@ -60,7 +60,9 @@ public class HeadsUpSettings extends SettingsPreferenceFragment implements
 
     private static final int DIALOG_BLACKLIST_APPS = 0;
     private static final int DIALOG_WHITELIST_APPS = 1;
+    private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
 
+    private ListPreference mHeadsUpTimeOut;
     private PackageListAdapter mPackageAdapter;
     private PackageManager mPackageManager;
     private PreferenceGroup mBlacklistPrefList;
@@ -93,6 +95,21 @@ public class HeadsUpSettings extends SettingsPreferenceFragment implements
         mAddBlacklistPref.setOnPreferenceClickListener(this);
         mAddWhitelistPref.setOnPreferenceClickListener(this);
 
+        Resources systemUiResources;
+        try {
+            systemUiResources = getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (Exception e) {
+            return;
+        }
+
+        int defaultTimeOut = systemUiResources.getInteger(systemUiResources.getIdentifier(
+                    "com.android.systemui:integer/heads_up_notification_decay", null, null));
+        mHeadsUpTimeOut = (ListPreference) findPreference(PREF_HEADS_UP_TIME_OUT);
+        mHeadsUpTimeOut.setOnPreferenceChangeListener(this);
+        int headsUpTimeOut = Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_TIMEOUT, defaultTimeOut);
+        mHeadsUpTimeOut.setValue(String.valueOf(headsUpTimeOut));
+        updateHeadsUpTimeOutSummary(headsUpTimeOut);
     }
 
     @Override
@@ -362,7 +379,21 @@ public class HeadsUpSettings extends SettingsPreferenceFragment implements
         return MetricsProto.MetricsEvent.PIXELDUST;
     }
 
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHeadsUpTimeOut) {
+            int headsUpTimeOut = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_TIMEOUT, headsUpTimeOut);
+            updateHeadsUpTimeOutSummary(headsUpTimeOut);
+            return true;
+        }
         return false;
     }
+
+    private void updateHeadsUpTimeOutSummary(int value) {
+        String summary = getResources().getString(R.string.heads_up_time_out_summary,
+                value / 1000);
+        mHeadsUpTimeOut.setSummary(summary);
+     }
 }
