7d89c573801345f13ba450792c47f1fc5719b874
cristianomatos <cristianobmatos@gmail.com>
2016-12-07 14:50:41 +0800

    Recents Clear All Button Tweaks (squashed) [2/2]
    Change-Id: Id084af95982b8933c8632798112a12e12aff521b
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    PixelDust Edits: add required imports.
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/pixeldust/settings/fragments/RecentsSettings.java
+++ src/com/pixeldust/settings/fragments/RecentsSettings.java
@@ -2,18 +2,56 @@ package com.pixeldust.settings.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
-import com.android.settings.R;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 
+import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class RecentsSettings extends SettingsPreferenceFragment {
+public class RecentsSettings extends SettingsPreferenceFragment
+         implements OnPreferenceChangeListener {
+
+    private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+    private SwitchPreference mRecentsClearAll;
+    private ListPreference mRecentsClearAllLocation;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.pixeldust_settings_recents);
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        // clear all location
+        mRecentsClearAllLocation = (ListPreference) findPreference(RECENTS_CLEAR_ALL_LOCATION);
+        int location = Settings.System.getIntForUser(resolver,
+                Settings.System.RECENTS_CLEAR_ALL_LOCATION, 3, UserHandle.USER_CURRENT);
+        mRecentsClearAllLocation.setValue(String.valueOf(location));
+        mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
+        mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mRecentsClearAllLocation) {
+            int location = Integer.valueOf((String) newValue);
+            int index = mRecentsClearAllLocation.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
+            mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
+            return true;
+        }
+        return false;
     }
 
     @Override
