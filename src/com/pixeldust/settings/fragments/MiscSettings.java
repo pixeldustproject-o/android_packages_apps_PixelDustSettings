d8df8ed6c21b3959ec1cea7392bcad15b775d99f
spezi77 <spezi77@gmx.com>
2018-02-03 23:46:00 +0100

    Revert "[2/2] Configure SystemUI theme."
    This reverts commit 0ab64f7cd60ba162793a322ed01ff85348e1d601.
    
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/pixeldust/settings/fragments/MiscSettings.java
+++ src/com/pixeldust/settings/fragments/MiscSettings.java
@@ -2,7 +2,6 @@ package com.pixeldust.settings.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
-import android.content.ContentResolver;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.os.UserHandle;
@@ -19,37 +18,15 @@ import com.android.settings.SettingsPreferenceFragment;
 public class MiscSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private static final String SYSTEMUI_THEME_STYLE = "systemui_theme_style";
-    private ListPreference mSystemUIThemeStyle;
-
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.pixeldust_settings_misc);
-        ContentResolver resolver = getActivity().getContentResolver();
-
-        mSystemUIThemeStyle = (ListPreference) findPreference(SYSTEMUI_THEME_STYLE);
-        int systemUIThemeStyle = Settings.System.getInt(resolver,
-                Settings.System.SYSTEM_UI_THEME, 0);
-        int valueIndex = mSystemUIThemeStyle.findIndexOfValue(String.valueOf(systemUIThemeStyle));
-        mSystemUIThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
-        mSystemUIThemeStyle.setSummary(mSystemUIThemeStyle.getEntry());
-        mSystemUIThemeStyle.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
-
-        if (preference == mSystemUIThemeStyle) {
-            String value = (String) objValue;
-            Settings.System.putInt(resolver,
-                    Settings.System.SYSTEM_UI_THEME, Integer.valueOf(value));
-            int valueIndex = mSystemUIThemeStyle.findIndexOfValue(value);
-            mSystemUIThemeStyle.setSummary(mSystemUIThemeStyle.getEntries()[valueIndex]);
-            return true;
-         }
         return false;
     }
 
